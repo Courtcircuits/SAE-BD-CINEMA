0bdc107961b998bf5ef2f0b7e247293fca18b5e3
Radulescu Tristan-Mihai <tristan-mihai.radulescu@etu.umontpellier.fr>
2022-05-08 13:37:11 +0200

    Ajout Categorie.sql et COmmune.sql

---
+++ creation/Categorie.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE Categorie(
+    idCategorie INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nomCategorie VARCHAR(50),
+    nomTypeCategorie VARCHAR(50),
+    FOREIGN KEY(nomTypeCategorie) REFERENCES TypeCategorie(nomTypeCategorie)
+)
---
+++ creation/Commune.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Commune(
+    numCommune INT PRIMARY KEY NOT NULL,
+    nomCommune VARCAHR(50) NOT NULL,
+    numDepartement VARCHAR(10) NOT NULL,
+    FOREIGN KEY(numDepartement) REFERENCES Departement(numDepartement)
+)
+
