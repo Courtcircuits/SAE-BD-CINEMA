0fe33e343d23cddb5f3e5ce06dc4754d683238e2
Radulescu Tristan-Mihai <tristan-mihai.radulescu@etu.umontpellier.fr>
2022-05-08 15:02:30 +0200

    Script creation historique

---
+++ creation/HistoriqueDep.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE HistoriqueDep(
+    annee INT PRIMARY KEY NOT NULL,
+    numDepartement VARCHAR(10) PRIMARY KEY NOT NULL,
+    nbEcransAE INT,
+    nbFauteuilsAE INT,
+    nbSeancesAE INT,
+    nbEntreesAE INT,
+    recettesAE INT,
+    FOREIGN KEY(annee) REFERENCES Annee(annee),
+    FOREIGN KEY(numDepartement) REFERENCES Departement(numDepartement)
+)
---
+++ creation/HistoriqueUniteUrbaine.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE HistoriqueUniteUrbaine(
+    annee INT PRIMARY KEY NOT NULL,
+    numUniteUrbaine VARCHAR(10) PRIMARY KEY NOT NULL,
+    nbEcransAE INT,
+    nbFauteuilsAE INT ,
+    nbSeancesAE INT,
+    nbEntreesAE INT,
+    recettesAE INT,
+    recette INT,
+    FOREIGN KEY (annee) REFERENCES Annee(annee),
+    FOREIGN KEY (numUniteUrbaine) REFERENCES UniteUrbaine(numUniteUrbaine)
+)
