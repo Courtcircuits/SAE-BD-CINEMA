70c80ea8d76298ddf9810dbc58a99ce5ed77152a
Radulescu Tristan-Mihai <tristan-mihai.radulescu@etu.umontpellier.fr>
2022-05-08 15:21:35 +0200

    Ajout creation Public

---
+++ creation/Public.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Public(
+    annee INT PRIMARY KEY NOT NULL,
+    nomRegion VARCHAR(50) PRIMARY KEY NOT NULL,
+    idCategorie INT PRIMARY KEY NOT NULL,
+    pourcentage FLOAT,
+    FOREIGN KEY(annee) REFERENCES Annee(annee),
+    FOREIGN KEY(nomRegion) REFERENCES Region(nomRegion),
+    FOREIGN KEY(idCategorie) REFERENCES Categorie(idCategorie)
+)
