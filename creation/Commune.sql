2253dc5fb32e3350672ad6b06254e133ea7183ae
Radulescu Tristan-Mihai <tristan-mihai.radulescu@etu.umontpellier.fr>
2022-05-08 13:41:10 +0200

    Modification Commune et creation HistoriqueCommune

--- creation/Commune.sql
+++ creation/Commune.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE Commune(
-    numCommune INT PRIMARY KEY NOT NULL,
+    numCommune INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nomCommune VARCAHR(50) NOT NULL,
     numDepartement VARCHAR(10) NOT NULL,
     FOREIGN KEY(numDepartement) REFERENCES Departement(numDepartement)
---
+++ creation/HistoriqueCommune.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE HistoriqueCommune(
+    numCommune INT PRIMARY KEY NOT NULL,
+    annee INT PRIMARY KEY NOT NULL,
+    populations INT,
+    nbEtablissement INT,
+    nbEcrans INT,
+    nbFauteuils INT,
+    nbMultiplexes INT,
+    nbCinemasArtEssai INT,
+    nbSeances INT,
+    nbEntrees INT,
+    recettes INT,
+    FOREIGN KEY (numCommune) REFERENCES Commune(numCommune),
+    FOREIGN KEY (annee) REFERENCES Annee(annee)
+)
