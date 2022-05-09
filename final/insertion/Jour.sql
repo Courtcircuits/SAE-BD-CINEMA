af8fe4d1453100ef463ab0584f76904d8dbe8bc5
Milou <90451752+Milou666@users.noreply.github.com>
2022-05-09 22:49:01 +0200

    Update Jour.sql

--- final/insertion/Jour.sql
+++ final/insertion/Jour.sql
@@ -1,7 +1,7 @@
-﻿INSERT INTO Jour(nationalite) VALUES ('lundi');
-INSERT INTO Jour(nationalite) VALUES ('mardi');
-INSERT INTO Jour(nationalite) VALUES ('mercredi');
-INSERT INTO Jour(nationalite) VALUES ('jeudi');
-INSERT INTO Jour(nationalite) VALUES ('vendredi');
-INSERT INTO Jour(nationalite) VALUES ('samedi');
-INSERT INTO Jour(nationalite) VALUES ('dimanche');
+INSERT INTO Jour(jour) VALUES ('lundi');
+INSERT INTO Jour(jour) VALUES ('mardi');
+INSERT INTO Jour(jour) VALUES ('mercredi');
+INSERT INTO Jour(jour) VALUES ('jeudi');
+INSERT INTO Jour(jour) VALUES ('vendredi');
+INSERT INTO Jour(jour) VALUES ('samedi');
+INSERT INTO Jour(jour) VALUES ('dimanche');
