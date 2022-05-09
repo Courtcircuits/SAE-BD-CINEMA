6404a5951f436f6c46563d1dc47f8d367b56aed6
Milou <90451752+Milou666@users.noreply.github.com>
2022-05-09 22:47:02 +0200

    Update TypeCategorie.sql

--- final/insertion/TypeCategorie.sql
+++ final/insertion/TypeCategorie.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO TypeCategorie(nomTypeFilm) VALUES ('sexe');
-INSERT INTO TypeCategorie(nomTypeFilm) VALUES ('age');
-INSERT INTO TypeCategorie(nomTypeFilm) VALUES ('categorie socioprofessionnelle');
-INSERT INTO TypeCategorie(nomTypeFilm) VALUES ('habitudes');
+INSERT INTO TypeCategorie(nomTypeCategorie) VALUES ('sexe');
+INSERT INTO TypeCategorie(nomTypeCategorie) VALUES ('age');
+INSERT INTO TypeCategorie(nomTypeCategorie) VALUES ('categorie socioprofessionnelle');
+INSERT INTO TypeCategorie(nomTypeCategorie) VALUES ('habitudes');
